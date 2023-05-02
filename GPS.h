43a0c6d58b42d65454db72f9383f32d68f00b844
PraisedChild <132355393+PraisedChild@users.noreply.github.com>
2023-05-03 00:07:34 +0300

    Update GPS.h

--- GPS.h
+++ GPS.h
@@ -1,1 +1,6 @@
-#define PI 3.141592653589793238 
+#define PI 3.141592653589793238
+void GPS_format();
+void GPS_read();
+float ToDegree (float angle);
+float ToRad (float angle);
+float GPS_getDistance(float currentLong , float currentLat , float destLong , float destLat);

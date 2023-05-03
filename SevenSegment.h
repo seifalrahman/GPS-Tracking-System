e52cbebe08ce302c5247cf6b7f5e26ebd8010fb4
ayosry <82183592+ayosry@users.noreply.github.com>
2023-05-03 12:36:28 +0300

    Update SevenSegment.h

--- SevenSegment.h
+++ SevenSegment.h
@@ -1,6 +1,13 @@
-#include "GPIO.h"
-#include "D:\keil 4\tm4c123gh6pm.h"
-#include "D:\keil 4\bit_utilies.h"
+#ifndef SevenSegment
+#define SevenSegment
 
-void SevenSegment_Set (unsigned char Value);
+#include "GPIO.h"
+#include "tm4c123gh6pm.h"
+#include "bit_utilies.h"
+
+void SevenSegment001_Set (unsigned char Value);
+void SevenSegment010_Set (unsigned char Value);
+void SevenSegment100_Set (unsigned char Value);
 void Split (double distance);
+
+#endif

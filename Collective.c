d07c8e58b66d06dd245718453f4a37278db0cc90
PraisedChild <132355393+PraisedChild@users.noreply.github.com>
2023-05-03 21:47:04 +0300

    Update Collective.c

--- Collective.c
+++ Collective.c
@@ -83,9 +83,9 @@ void Split (double distance){
  //***********************LED.c**********************
  void LED_Setup(){
 	SYSCTL_RCGCGPIO_R |= 0x20;
-	while(~(SYSCTL_RCGCGPIO_R &= 0x20));
+	while(!(SYSCTL_PRGPIO_R & 0x20));
 	GPIO_PORTF_LOCK_R= 0x4C4F434B;
-	GPIO_PORTF_CR_R= 0x01;
+	GPIO_PORTF_CR_R= 0xFF;
 	GPIO_PORTF_AMSEL_R &= ~0x0E;
 	GPIO_PORTF_DEN_R |= 0x0E;
 	GPIO_PORTF_DIR_R |= 0x0E;

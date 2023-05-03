03a810d685bff0aca53eece435c89d2b40834343
ayosry <82183592+ayosry@users.noreply.github.com>
2023-05-03 12:13:41 +0300

    Update GPIO.h

--- GPIO.h
+++ GPIO.h
@@ -1,3 +1,27 @@
 
-void GPIO_InitB ();
+
+///PortF switches
+#define GPIO_SW1 0
+#define GPIO_SW2 1
+#define GPIO_SW_NOT_PRESSED 1
+
+///PortF LED color
+#define GPIO_RED_LED 0
+#define GPIO_GREEN_LED 1
+#define GPIO_BLUE_LED 2
+
+///PortF LED state ON/OFF
+#define GPIO_LED_OFF 0
+#define GPIO_LED_ON 1
+
+
+
+
+////////Functions
+void GPIOB_Init ();
 //void GPIO_PORTB_set (unsigned char Value);
+void GPIOF_Init();
+void GPIOA_Init ();
+void GPIOD_Init ();
+void setLedValue(unsigned char ledColor, unsigned char ledState);
+unsigned char GPIO_getSwitchValue (unsigned char sw);

ada8970c250db2b003f1e17469ee8cd2abf1c9ed
mostafa0110 <73003302+mostafa0110@users.noreply.github.com>
2023-05-07 03:02:14 +0300

    Update SevenSegment.h

--- SevenSegment.h
+++ SevenSegment.h
@@ -6,10 +6,16 @@
 #include "tm4c123gh6pm.h"
 #include "bit_utilities.h"
 
-static unsigned char SevenSegmentArr[10]={0x3F,0x6,0x5B,0x4F,0x66,0x6D,0x7D,0x27,0x7F,0x6F};
+static unsigned char SevenSegmentArr[10]={0x40,0x79,0x24,0x30,0x19,0x12,0x02,0x78,0x00,0x10}; // common anode
 
-void SevenSegment001_Set (unsigned char Value);
-void SevenSegment010_Set (unsigned char Value);
-void SevenSegment100_Set (unsigned char Value);
+void SevenSegment_Set (unsigned char Value);
+void Split (double distance);
+//void SevenSegment001_Set (unsigned char Value);
+//void SevenSegment010_Set (unsigned char Value);
+//void SevenSegment100_Set (unsigned char Value);
+void display(int digit,char sig);
+void Sys_tick_Init();
+void SysTick_Handler();
+
 
 #endif

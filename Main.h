435a12657eedbcb86bb7ab2ac9ff7516fdb451d6
Hussein Assaad <132515456+MiserableChild@users.noreply.github.com>
2023-05-13 22:21:36 +0300

    Delete Main.h

--- Main.h
+++
@@ -1,9 +0,0 @@
-//#include "tm4c123gh6pm.h"
-//#include "bit_utilities.h"
-//#include <string.h>
-//#include <stdlib.h>
-//#include <math.h> 
-
-#include "GPS.h"
-#include "LED.h"
-#include "SevenSegment.h"
